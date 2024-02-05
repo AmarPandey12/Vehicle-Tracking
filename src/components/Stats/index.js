6b840c7ecef35c490ffc7a7c88321ed3bc949103
Amar Nath Pandey <amar.61039035@ltimindtree.com>
2024-02-05 16:54:40 +0530

    home updates

--- src/components/Stats/index.js
+++ src/components/Stats/index.js
@@ -9,7 +9,7 @@ function Stats() {
 
     const [eid, setEid] = useState('');
     // const token = '5560db8f4a2317dfb4690c5273a984098D637F85A0851AC11CC6D0450DC3A9CFB006FA40';
-    const API = `https://vehicle-tracking-server-production.up.railway.app/getVehicles?sid=0460b311bb8374a3dd31018c4ee7719d&b=stable&v=1.51`
+    const API = `https://vehicle-tracking-server-production.up.railway.app/getVehicles?sid=04cdeddc25b30cbef42afde1bd626e93&b=stable&v=1.51`
 
     // var payload = JSON.stringify({
     //     token: token
